dd8c1894be760bd3326f29127592dd4f9056438f
ilhaha <644452492@qq.com>
2025-10-15 17:07:40 +0800

    'init'

---
+++ src/views/login/components/background/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="login-bg">
+    <div class="fly bg-fly-circle1"></div>
+    <div class="fly bg-fly-circle2"></div>
+    <div class="fly bg-fly-circle3"></div>
+    <div class="fly bg-fly-circle4"></div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+
+</style>
